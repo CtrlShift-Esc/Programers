6ca705c6a4a24303f7852dcb34e15c2e91cdec4c
CtrlShift-Esc <jehovah0427@gmail.com>
2022-10-06 16:16:31 +0900

    add

---
+++ lv0/c++/두 수의 나눗셈.cpp
@@ -0,0 +1,10 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/120806
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = ((double)num1/(double)num2)*1000;
+    return answer;
+}
