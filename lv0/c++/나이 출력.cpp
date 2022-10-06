5abf9e7fd73b40c39568d2bd5899523a089086c7
CtrlShift-Esc <jehovah0427@gmail.com>
2022-10-06 16:09:14 +0900

    add

---
+++ lv0/c++/나이 출력.cpp
@@ -0,0 +1,10 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/120820
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int age) {
+    int answer = 2022-age+1;
+    return answer;
+}
